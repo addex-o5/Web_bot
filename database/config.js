6dfd24f5239af6ab0e4aa4c358036756061e70a9
addex-o5 <151564873+addex-o5@users.noreply.github.com>
2025-11-15 23:45:45 +0100

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+// ©AiiSigma
+
+module.exports = {
+  domain: "sftp://zekejrdev2.k0mraidhost.name.ng:2026", // Ganti sma domain panel lu dan pastiin harus *http*
+  port: "45578" // Ganti Portnoy di panel network.
+};
